dd487962d8b1538f4475deeeee679893aaecfbe6
j0Shi82 <janosch.ferda@e-domizil.de>
2020-08-16 01:47:50 +0200

    accept multiple tags in aggregate discussion endpoint

--- src/Controller/V1/Infoaggregates/Discussion.php
+++ src/Controller/V1/Infoaggregates/Discussion.php
@@ -29,8 +29,17 @@ class Discussion extends BaseController
 
         $data_ary = array(
             'limit' => $this->requestHelper->variable('limit', 50),
-            'tag' => $this->requestHelper->variable('tag', 0),
+            'tags' => $this->requestHelper->variable('tags', ''),
         );
+
+        if ($data_ary['tags'] !== '') {
+            $data_ary['tags'] = explode(",", $data_ary['tags']);
+            foreach ($data_ary['tags'] as &$tag) {
+                $tag = intval($tag);
+            }
+        } else {
+            $data_ary['tags'] = [];
+        }
 
         if ($data_ary['limit'] > 100) {
             $data_ary['limit'] = 100;
@@ -39,14 +48,17 @@ class Discussion extends BaseController
             $data_ary['limit'] = 20;
         }
 
-        if ($data_ary['tag'] !== 0) {
+        if (count($data_ary['tags']) > 0) {
             $sql = "
-                SELECT a.site, a.link, a.title FROM article as a, article_tags as atags, tag as t
+                SELECT a.site, a.link, a.title, COUNT(*) as count
+                FROM article as a, article_tags as atags, tag as t
                 WHERE 
                     atags.article_id = a.id 
                     AND atags.tag_id = t.id 
                     AND a.type = 'discussion'
-                    AND t.id = " . $data_ary['tag'] . "
+                    AND t.id IN (" . implode(",", $data_ary['tags']) . ")
+                GROUP BY a.id
+                HAVING count >= " . count($data_ary['tags']) . "
                 ORDER BY a.ts DESC
                 LIMIT " . $data_ary['limit'] . "
             ";
@@ -54,7 +66,13 @@ class Discussion extends BaseController
             $sql = "SELECT a.site, a.link, a.title FROM article as a WHERE a.type = 'discussion' ORDER BY a.ts DESC LIMIT " . $data_ary['limit'];
         }
 
-        $response->getBody()->write(json_encode($this->db->sql_fetch_array($sql)));
+        $results = $this->db->sql_fetch_array($sql);
+        foreach ($results as &$row) {
+            $row['title'] = html_entity_decode($row['title']);
+            $row[2] = html_entity_decode($row[2]);
+        }
+
+        $response->getBody()->write(json_encode($results));
         return $response
             ->withHeader('Content-Type', 'application/json')
             ->withHeader('charset', 'utf-8');
